4da7248e66c840ae85c04ff714eba82e408b2a06
srikanto5120 <shreekantaray@gmail.com>
2021-09-06 04:52:30 +0600

    complate

--- stop.js
+++ stop.js
@@ -1,21 +1,25 @@
-let hr = 0;
-let min = 0;
-let sec = 0;
-let count = 0;
-let timer = true;
+var hr = 0;
+var min = 0;
+var sec = 0;
+var count = 0;
+var timer = true;
 const star = () => {
   timer = true;
   stopWatch("timer");
 };
 const stop = () => {
   timer = false;
+  hr = 0;
+  min = 0;
+  sec = 0;
+  count = 0;
 };
 const reset = () => {
   timer = false;
-  document.getElementById("hr").innerHTML = "0";
-  document.getElementById("min").innerHTML = "0";
-  document.getElementById("sec").innerHTML = "0";
-  document.getElementById("count").innerHTML = "0";
+  document.getElementById("hr").innerHTML = "00";
+  document.getElementById("min").innerHTML = "00";
+  document.getElementById("sec").innerHTML = "00";
+  document.getElementById("count").innerHTML = "00";
 };
 
 const stopWatch = () => {
@@ -34,19 +38,28 @@ const stopWatch = () => {
         }
       }
     }
-    let hrString = hr;
-    let minString = min;
-    let secString = sec;
-    let counString = count;
+
+    var hrString = hr;
+    var minString = min;
+    var secString = sec;
+    var countString = count;
     if (hr < 10) {
       hrString = "0" + hrString;
+    }
+    if (min < 10) {
       minString = "0" + minString;
+    }
+    if (sec < 10) {
       secString = "0" + secString;
     }
+    if (count < 10) {
+      countString = "0" + countString;
+    }
 
+    document.getElementById("hr").innerHTML = hrString;
     document.getElementById("min").innerHTML = minString;
     document.getElementById("sec").innerHTML = secString;
-    document.getElementById("count").innerHTML = count;
+    document.getElementById("count").innerHTML = countString;
     setTimeout("stopWatch()", 10);
   }
 };
